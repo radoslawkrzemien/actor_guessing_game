415bb9572ed08e15bb3d6eb31cebb7b2af708f09
costrella <michal.kostrzewa@comarch.pl>
2015-12-08 10:52:15 +0100

    Added new actors and directors to prolog database

--- ActorGuessingGame/baza.pl
+++ ActorGuessingGame/baza.pl
@@ -20,6 +20,129 @@ gral_w(helena_bonham_carter,the_kings_speech).
 gral_w(helena_bonham_carter,sweeney_todd).
 gral_w(helena_bonham_carter,harry_potter_and_the_order_of_phoenix).
 
+gral_w(jack_nicholson, the_shining).
+gral_w(jack_nicholson, the_departed).
+gral_w(jack_nicholson, one_flew_over_the_cuckoos_nest).
+gral_w(jack_nicholson, chinatown).
+gral_w(jack_nicholson, five_easy_pieces).
+gral_w(jack_nicholson, professione_reporter).
+gral_w(marlon_brando, the_godfather).
+gral_w(marlon_brando, apocalypse_now).
+gral_w(marlon_brando, you_rock_my_world).
+gral_w(marlon_brando, the_young_lions).
+gral_w(marlon_brando, on_the_waterfront).
+gral_w(daniel_day_lewis, my_brother_jonathan).
+gral_w(daniel_day_lewis, lincoln).
+gral_w(daniel_day_lewis, my_left_foot).
+gral_w(daniel_day_lewis, there_will_be_blood).
+gral_w(dustin_hoffman, rain_man).
+gral_w(dustin_hoffman, midnight_cowboy).
+gral_w(dustin_hoffman, kramer_vs_kramer).
+gral_w(dustin_hoffman, little_big_man).
+gral_w(dustin_hoffman, papillon).
+gral_w(dustin_hoffman, sleepers).
+gral_w(tom_hanks, forrest_gump).
+gral_w(tom_hanks, philadelphia).
+gral_w(tom_hanks, the_green_mile).
+gral_w(tom_hanks, band_of_brothers).
+gral_w(tom_hanks, saving_private_ryan).
+gral_w(tom_hanks, catch_me_if_you_can).
+gral_w(tom_hanks, apollo13).
+gral_w(tom_hanks, cast_away).
+gral_w(denzel_washington, man_on_fire).
+gral_w(denzel_washington, philadelphia).
+gral_w(denzel_washington, american_gangster).
+gral_w(denzel_washington, inside_man).
+gral_w(denzel_washington, training_day).
+gral_w(denzel_washington, the_bone_collector).
+gral_w(denzel_washington, flight).
+gral_w(denzel_washington, deja_vu).
+gral_w(denzel_washington, unstoppable).
+gral_w(michael_caine, inception).
+gral_w(michael_caine, the_prestige).
+gral_w(michael_caine, interstellar).
+gral_w(michael_caine, the_dark_knight).
+gral_w(michael_caine, the_dark_knight_rises).
+gral_w(michael_caine, batman_begins).
+gral_w(michael_caine, educated_rita).
+gral_w(morgan_freeman, the_shawshank_redemption).
+gral_w(morgan_freeman, seven).
+gral_w(morgan_freeman, unforgiven).
+gral_w(morgan_freeman, the_dark_knight).
+gral_w(morgan_freeman, the_dark_knight_rises).
+gral_w(morgan_freeman, million_dolar_baby).
+gral_w(morgan_freeman, lucky_number_slevin).
+gral_w(morgan_freeman, batman_begins).
+gral_w(clint_eastwood, unforgiven).
+gral_w(clint_eastwood, million_dolar_baby).
+gral_w(clint_eastwood, the_good_the_bad_and_the_ugly).
+gral_w(clint_eastwood, gran_torino).
+gral_w(clint_eastwood, dirty_harry).
+gral_w(clint_eastwood, magnum_force).
+gral_w(clint_eastwood, kellys_heroes).
+gral_w(clint_eastwood, escape_from_alcatraz).
+gral_w(clint_eastwood, a_perfect_world).
+gral_w(robert_duvall, the_godfather).
+gral_w(robert_duvall, apocalypse_now).
+gral_w(robert_duvall, the_judge).
+gral_w(robert_duvall, falling_down).
+gral_w(russell_crowe, gladiator).
+gral_w(russell_crowe, a_beautiful_mind).
+gral_w(russell_crowe, american_gangster).
+gral_w(russell_crowe, cinderella_man).
+gral_w(russell_crowe, the_next_three_days).
+gral_w(russell_crowe, robin_hood).
+gral_w(robin_williams, august_rush).
+gral_w(robin_williams, dead_poets_society).
+gral_w(robin_williams, awakenings).
+gral_w(robin_williams, good_Will_hunting).
+gral_w(robin_williams, jumanji).
+gral_w(robin_williams, the_prince_of_providence).
+gral_w(johny_depp, pirates_of_the_caribbean).
+gral_w(johny_depp, pluton).
+gral_w(johny_depp, edward_scissorhands).
+gral_w(johny_depp, public_enemies).
+gral_w(leonardo_dicaprio, the_wolf_of_wall_street).
+gral_w(leonardo_dicaprio, django).
+gral_w(leonardo_dicaprio, inception).
+gral_w(leonardo_dicaprio, shutter_island).
+gral_w(leonardo_dicaprio, the_departed).
+gral_w(leonardo_dicaprio, catch_me_if_you_can).
+gral_w(leonardo_dicaprio, titanic).
+gral_w(leonardo_dicaprio, the_great_gatsby).
+gral_w(tommy_lee_jones, lincoln).
+gral_w(tommy_lee_jones, elizas_horoscope).
+gral_w(tommy_lee_jones, the_executioners_song).
+gral_w(tommy_lee_jones, jfk).
+gral_w(tommy_lee_jones, natural_born_killers).
+gral_w(tommy_lee_jones, the_fugitive).
+gral_w(kevin_spacey, seven).
+gral_w(kevin_spacey, american_beauty).
+gral_w(kevin_spacey, the_negotiator).
+gral_w(kevin_spacey, pay_it_forward).
+gral_w(kevin_spacey, a_time_of_kill).
+gral_w(christoph_waltz, django).
+gral_w(christoph_waltz, spectre).
+gral_w(christoph_waltz, inglourious_basterds).
+gral_w(christoph_waltz, spymaster).
+gral_w(christoph_waltz, big_eyes).
+gral_w(edward_norton, american_history_x).
+gral_w(edward_norton, fight_club).
+gral_w(edward_norton, primal_fear).
+gral_w(edward_norton, the_illusionist).
+gral_w(edward_norton, red_dragon).
+gral_w(christian_bale, the_prestige).
+gral_w(christian_bale, the_dark_knight).
+gral_w(christian_bale, the_dark_knight_rises).
+gral_w(christian_bale, batman_begins).
+gral_w(christian_bale, public_enemies).
+gral_w(liam_neeson, schindlers_list).
+gral_w(liam_neeson, the_mission).
+gral_w(liam_neeson, the_dark_knight_rises).
+gral_w(liam_neeson, taken).
+gral_w(liam_neeson, batman_begins).
+gral_w(liam_neeson, the_next_three_days).
+
 gral_postac(robert_de_niro,taxi_driver,glowny_bohater).
 gral_postac(robert_de_niro,taxi_driver,zwykly_czlowiek).
 gral_postac(robert_de_niro,taxi_driver,taksowkarz).
@@ -84,23 +207,74 @@ gral_postac(helena_bonham_carter,harry_potter_and_the_order_of_phoenix,czarny_ch
 gral_postac(helena_bonham_carter,harry_potter_and_the_order_of_phoenix,czarodziej).
 gral_postac(helena_bonham_carter,harry_potter_and_the_order_of_phoenix,przestepca).
 
+gral_w(jack_nicholson, the_shining, glowny_bohater).
+gral_w(jack_nicholson, the_shining, psychopata).
+gral_w(jack_nicholson, one_flew_over_the_cuckoos_nest, glowny_bohater).
+gral_w(jack_nicholson, one_flew_over_the_cuckoos_nest, glowny_bohater).
+gral_w(marlon_brando, the_godfather, czlowiek_wladzy).
+gral_w(marlon_brando, the_godfather, bogacz).
+gral_w(marlon_brando, the_godfather, biznesman).
+gral_w(tom_hanks, forrest_gump, glowny_bohater).
+gral_w(tom_hanks, the_green_mile, straznik).
+gral_w(tom_hanks, the_green_mile, dobra_postac).
+gral_w(tom_hanks, catch_me_if_you_can, detektyw).
+gral_w(tom_hanks, apollo13, kosmonauta).
+gral_w(tom_hanks, apollo13, dobra_postac).
+gral_w(tom_hanks, cast_away, glowny_bohater).
+gral_w(tom_hanks, cast_away, ocalaly).
+gral_w(tom_hanks, cast_away, samotnik).
+gral_w(tom_hanks, cast_away, dobra_postac).
+gral_w(denzel_washington, flight, glowny_bohater).
+gral_w(denzel_washington, flight, pilot).
+gral_w(denzel_washington, flight, alkoholik).
+gral_w(denzel_washington, training_day, policjant).
+
+
 %%	rezyserowal(Rezyser,Film) - Rezyser rezyserowal Film
 rezyserowal(martin_scorsese,taxi_driver).
 rezyserowal(martin_scorsese,raging_bull).
 rezyserowal(martin_scorsese,goodfellas).
+rezyserowal(martin_scorsese,the_departed).
 rezyserowal(michael_cimino,the_deer_hunter).
 rezyserowal(jonathan_demme,the_silence_of_the_lambs).
+rezyserowal(jonathan_demme,philadelphia).
 rezyserowal(martin_brest,meet_joe_black).
 rezyserowal(ridley_scott,hannibal).
+rezyserowal(ridley_scott,american_gangster).
+rezyserowal(ridley_scott,gladiator).
 rezyserowal(brett_ratner,red_dragon).
 rezyserowal(francis_ford_coppola,the_godfather).
+rezyserowal(francis_ford_coppola,apocalypse_now).
 rezyserowal(brian_de_palma,scarface).
 rezyserowal(michael_mann,heat).
 rezyserowal(martin_brest,scent_of_a_woman).
 rezyserowal(david_fincher,fight_club).
+rezyserowal(david_fincher,seven).
 rezyserowal(tom_hooper,the_kings_speech).
 rezyserowal(tim_burton,sweeney_todd).
 rezyserowal(david_yates,harry_potter_and_the_order_of_phoenix).
+rezyserowal(stanley_kubrick, the_shining).
+rezyserowal(milos_forman, one_flew_over_the_cuckoos_nest).
+rezyserowal(roman_polanski, chinatown).
+rezyserowal(bob_rafelson, the_five_easy_pieces).
+rezyserowal(robert_zemeckis, the_five_easy_pieces).
+rezyserowal(robert_zemeckis, cast_away).
+rezyserowal(frank_darabont, the_green_mile).
+rezyserowal(frank_darabont, the_shawshank_redemption).
+rezyserowal(steven_spielberg, catch_me_if_you_can).
+rezyserowal(ron_howard, apollo_13).
+rezyserowal(antoine_fuqua, training_day).
+rezyserowal(christopher_nolan, inception).
+rezyserowal(christopher_nolan, the_prestige).
+rezyserowal(christopher_nolan, interstellar).
+rezyserowal(christopher_nolan, the_dark_knight).
+rezyserowal(christopher_nolan, the_dark_knight_rises).
+rezyserowal(christopher_nolan, batman_begins).
+rezyserowal(clint_eastwood, the_million_dollar_baby).
+rezyserowal(david_dobkin,the_judge).
+rezyserowal(quentin_tarantiono,django).
+
+
 
 %%	film_z_serii(Film,Seria) - Film nalezy do Serii
 film_z_serii(harry_potter_and_the_order_of_phoenix,harry_potter_series).
